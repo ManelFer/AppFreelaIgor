61d3b31f083d46d0805b7d5ce32292ad8b4266a3
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2025-03-31 13:07:20 -0300

    back
    inicio

---
+++ formulario/backend-país/src/test/java/com/formulario/backend_pais/BackendPaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formulario.backend_pais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
