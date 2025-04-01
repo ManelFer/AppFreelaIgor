be6df2eb7e0905edcb10114bc5a2d3af8ce58c44
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2025-04-01 11:13:45 -0300

    desup
    deu erraado, irei refazer

--- formulario/backend-país/src/main/java/com/formulario/backend_pais/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.formulario.backend_pais.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
